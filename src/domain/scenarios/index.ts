f431659d02fb60f077d3b3228ca6c689cc041fd7
Akshat <akshat41121995@gmail.com>
2026-09-15 10:41:07 +0200

    CF-012: add pure scenario composition and contract

---
+++ src/domain/scenarios/index.ts
@@ -0,0 +1,10 @@
+export { composePropertyScenario } from './compose-property-scenario'
+export type {
+  PropertyScenario,
+  PropertyScenarioResult,
+  ScenarioBase,
+  ScenarioResultBase,
+  ScenarioStage,
+  TrackedScenarioBudget,
+  TrackedScenarioRate,
+} from './types'
