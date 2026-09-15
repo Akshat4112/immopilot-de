f9d2b62dbf5dc66f5bd1207f4d70aba0d55b874c
Akshat <akshat41121995@gmail.com>
2026-09-15 10:27:55 +0200

    CF-011: add index.ts

---
+++ src/domain/offer-price/index.ts
@@ -0,0 +1,12 @@
+export { calculateOfferPrice } from './calculate-offer-price'
+export { maximumOfferPriceSearchCents } from './types'
+export type {
+  AffordabilityCeilingResult,
+  AffordabilityOfferInput,
+  AvailableOfferPriceResult,
+  ComparableOfferInput,
+  ComparableOfferResult,
+  OfferPriceInput,
+  OfferPriceResult,
+  PriceCeilingResult,
+} from './types'
